ffe42f5e41b8d25d1340831612f48201e553bae6
Dmitry Dmitriev <d.dmitrieff@gmail.com>
2025-12-09 11:10:46 +0300

    update dialogs

---
+++ src/main/java/team/mephi/adminbot/model/Role.java
@@ -0,0 +1,24 @@
+package team.mephi.adminbot.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long code;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+}
