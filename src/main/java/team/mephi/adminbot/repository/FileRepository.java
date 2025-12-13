c993fc88aa9212a74f7dc20b90fbe9bd77adb7e5
Zhidkov-Aleksandr <dotenvargen@gmail.com>
2025-12-13 21:12:40 +0300

    Подправил интеграцию ошибки

--- src/main/java/team/mephi/adminbot/repository/FileRepository.java
+++ src/main/java/team/mephi/adminbot/repository/FileRepository.java
@@ -2,6 +2,7 @@ package team.mephi.adminbot.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import team.mephi.adminbot.model.StoredFile;
 
 @Repository
 public interface FileRepository extends JpaRepository<StoredFile, Long> {
