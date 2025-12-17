df7a8973e14641609ee7f04bc1dfbc4c0cbaa956
Dmitry Dmitriev <d.dmitrieff@gmail.com>
2025-12-17 17:16:52 +0300

    interface v2

--- src/main/java/team/mephi/adminbot/vaadin/views/Users.java
+++ src/main/java/team/mephi/adminbot/vaadin/views/Users.java
@@ -1,6 +1,5 @@
 package team.mephi.adminbot.vaadin.views;
 
-import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.html.Div;
@@ -11,26 +10,21 @@ import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.tabs.Tab;
 import com.vaadin.flow.component.tabs.TabSheet;
-import com.vaadin.flow.data.binder.BeanValidationBinder;
 import com.vaadin.flow.router.Route;
 import jakarta.annotation.security.RolesAllowed;
-import team.mephi.adminbot.dto.RoleDto;
-import team.mephi.adminbot.dto.SimpleUser;
-import team.mephi.adminbot.repository.RoleRepository;
-import team.mephi.adminbot.repository.TutorRepository;
-import team.mephi.adminbot.repository.UserRepository;
-import team.mephi.adminbot.vaadin.components.UserCountBadge;
 import team.mephi.adminbot.vaadin.components.UserConfirmDialog;
-import team.mephi.adminbot.vaadin.components.RightDrawer;
-import team.mephi.adminbot.vaadin.components.UserForm;
-import team.mephi.adminbot.vaadin.providers.ProviderGet;
-import team.mephi.adminbot.vaadin.views.users.*;
+import team.mephi.adminbot.vaadin.components.UserCountBadge;
+import team.mephi.adminbot.vaadin.users.components.RoleService;
+import team.mephi.adminbot.vaadin.users.components.UserEditorDialog;
+import team.mephi.adminbot.vaadin.users.dataproviders.UserDataProvider;
+import team.mephi.adminbot.vaadin.users.service.UserCountService;
+import team.mephi.adminbot.vaadin.users.service.UsersPresenter;
+import team.mephi.adminbot.vaadin.users.actions.UserActions;
+import team.mephi.adminbot.vaadin.users.tabs.UserTabProvider;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.stream.Collectors;
+import java.util.*;
 
 @Route("/users")
 @RolesAllowed("ADMIN")
@@ -57,152 +51,124 @@ public class Users extends VerticalLayout {
     private final String REJECT_ALL_MESSAGE = "Информация об отказе отправлена %d кандидатам";
     private final String REJECT_ACTION = "Отклонить";
 
-    RightDrawer driver;
-    UserConfirmDialog dialogBlock = new UserConfirmDialog(BLOCK_TITLE, BLOCK_TEXT, BLOCK_ACTION, BLOCK_ALL_TITLE, BLOCK_ALL_TEXT, this::showBlockMessage);
-    UserConfirmDialog dialogAccept = new UserConfirmDialog(ACCEPT_TITLE, ACCEPT_TEXT, ACCEPT_ACTION, ACCEPT_ALL_TITLE, ACCEPT_ALL_TEXT, this::showAcceptMessage);
-    UserConfirmDialog dialogReject = new UserConfirmDialog(REJECT_TITLE, REJECT_TEXT, REJECT_ACTION, REJECT_ALL_TITLE, REJECT_ALL_TEXT,  this::showRejectMessage);
-    Map<String, Long> roleCounts;
-    List<Component> tables;
-    TabSheet tabSheet;
-    ProviderGet provider;
-    List<Long> blockIds;
+    private final UserEditorDialog editorDialog;
+    private final UserConfirmDialog dialogBlock;
+    private final UserConfirmDialog dialogAccept;
+    private final UserConfirmDialog dialogReject;
+    private final TabSheet tabSheet = new TabSheet();
+    private final List<String> rolesInOrder = new ArrayList<>();
+    private final Map<String, UserDataProvider> dataProviders = new HashMap<>();
 
-    private final BeanValidationBinder<SimpleUser> binder = new BeanValidationBinder<>(SimpleUser.class);
+    public Users(
+            List<UserTabProvider> tabProviders,
+            UsersPresenter presenter,
+            RoleService roleService,
+            UserCountService userCountService
+    ) {
+        this.editorDialog = new UserEditorDialog(roleService);
+        this.dialogBlock = new UserConfirmDialog(BLOCK_TITLE, BLOCK_TEXT, BLOCK_ACTION, BLOCK_ALL_TITLE, BLOCK_ALL_TEXT, null);
+        this.dialogAccept = new UserConfirmDialog(ACCEPT_TITLE, ACCEPT_TEXT, ACCEPT_ACTION, ACCEPT_ALL_TITLE, ACCEPT_ALL_TEXT, null);
+        this.dialogReject = new UserConfirmDialog(REJECT_TITLE, REJECT_TEXT, REJECT_ACTION, REJECT_ALL_TITLE, REJECT_ALL_TEXT, null);
+        setSizeFull();
+        tabSheet.setSizeFull();
+        add(createHeader(), tabSheet, editorDialog);
+        tabProviders.sort(Comparator.comparingInt(UserTabProvider::getPosition));
+        // Создаём вкладки
+        for (var provider : tabProviders) {
+            var tabId = provider.getTabId();
+            var dataProvider = presenter.createDataProvider(tabId);
+            var actions = createActions(tabId, dataProvider);
+            var content = provider.createTabContent(actions);
+            rolesInOrder.add(tabId);
+            dataProviders.put(tabId, dataProvider);
+            var userCount = userCountService.getAllCounts().getOrDefault(provider.getTabId(), 0L);
+            Span tabContent = new Span(new Span(provider.getTabLabel()), new UserCountBadge(userCount));
+            tabSheet.add(new Tab(tabContent), content, provider.getPosition());
+        }
+    }
 
-    public Users(UserRepository userRepository, TutorRepository tutorRepository, RoleRepository roleRepository) {
-        List<String> tabs = List.of("visitor", "candidate", "middle_candidate", "student", "free_listener", "lc_expert", "tutor");
-        List<String> tabNames = List.of("Гости", "Кандидаты", "Миддл-Кандидаты", "Студенты", "Слушатели", "Эксперты", "Кураторы");
+    private UserActions createActions(String role, UserDataProvider dataProvider) {
+        return new UserActions() {
+            @Override
+            public void onView(Long id) {
+                dataProvider.findUserById(id).ifPresent(editorDialog::openForView);
+            }
+            @Override
+            public void onEdit(Long id) {
+                dataProvider.findUserById(id).ifPresent(u -> {
+                    editorDialog.openForEdit(u);
+                    editorDialog.setOnSaveCallback(() -> {
+                        dataProvider.save(editorDialog.getEditedUser());
+                        dataProvider.refresh();
+                    });
+                });
+            }
+            @Override
+            public void onDelete(List<Long> ids) {
+                dialogBlock.setCount(ids.size());
+                dialogBlock.setOnConfirm(() -> {
+                    dataProvider.deleteAllById(ids);
+                    dataProvider.refresh();
+                    if (ids.size() == 1)
+                        Notification.show(BLOCK_MESSAGE, 3000, Notification.Position.TOP_END);
+                    else if (ids.size() > 1) {
+                        Notification.show(String.format(BLOCK_ALL_MESSAGE, ids.size()), 3000, Notification.Position.TOP_END);
+                    }
+                });
+                dialogBlock.open();
+            }
+            @Override
+            public void onAccept(List<Long> ids) {
+                dialogAccept.setCount(ids.size());
+                dialogAccept.setOnConfirm(() -> {
+                    if (ids.size() == 1)
+                        Notification.show(ACCEPT_MESSAGE, 3000, Notification.Position.TOP_END);
+                    else if (ids.size() > 1) {
+                        Notification.show(String.format(ACCEPT_ALL_MESSAGE, ids.size()), 3000, Notification.Position.TOP_END);
+                    }
+                });
+                dialogAccept.open();
+            }
+            @Override
+            public void onReject(List<Long> ids) {
+                dialogReject.setCount(ids.size());
+                dialogReject.setOnConfirm(() -> {
+                    if (ids.size() == 1)
+                        Notification.show(REJECT_MESSAGE, 3000, Notification.Position.TOP_END);
+                    else if (ids.size() > 1) {
+                        Notification.show(String.format(REJECT_ALL_MESSAGE, ids.size()), 3000, Notification.Position.TOP_END);
+                    }
+                });
+                dialogReject.open();
+            }
+        };
+    }
 
-        setHeightFull();
+    private HorizontalLayout createHeader() {
         HorizontalLayout top = new HorizontalLayout();
         top.setWidthFull();
         top.addToStart(new H1("Пользователи"));
         var primaryButton = new Button("Добавить пользователя", new Icon(VaadinIcon.PLUS), e -> {
-            var selectedTab = tabSheet.getSelectedIndex();
-            var user = new SimpleUser();
-            user.setRole(tabs.get(selectedTab));
-            binder.setBean(user);
-            provider = (ProviderGet) tables.get(selectedTab);
-            driver.setVisible(true);
+            editorDialog.openForNew(getCurrentRole());
+            editorDialog.setOnSaveCallback(() -> {
+                var dataProvider = dataProviders.get(getCurrentRole());
+                dataProvider.save(editorDialog.getEditedUser());
+                dataProvider.refresh();
+            });
         });
         primaryButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
         Div buttons = new Div(new Button("Загрузить из файла", new Icon(VaadinIcon.FILE_ADD)), primaryButton);
         buttons.getElement().getStyle().set("display", "flex");
         buttons.getElement().getStyle().set("gap", "24px");
         top.addToEnd(buttons);
-
-        tabSheet = new TabSheet();
-        tabSheet.setSizeFull();
-
-        roleCounts = userRepository.countsByRole();
-
-        Map<String, UserCountBadge> badges = tabs.stream().collect(Collectors.toMap(
-                s -> s, key -> new UserCountBadge(roleCounts.getOrDefault(key, 0L))));
-
-        tables = List.of(
-                new GuestsView(userRepository, roleRepository, "visitor", this::onView, this::onBlock),
-                new CandidateView(userRepository, roleRepository, "candidate", this::onView, this::onEdit, this::onBlock, this::onAccept, this::onReject),
-                new MiddleCandidateView(userRepository, roleRepository, "middle_candidate", this::onView, this::onEdit, this::onBlock),
-                new StudentView(userRepository, roleRepository, "student", this::onView, this::onEdit, this::onBlock),
-                new FreeListenerView(userRepository, roleRepository, "free_listener", this::onView, this::onEdit, this::onBlock),
-                new ExpertsView(userRepository, roleRepository, "lc_expert", this::onView, this::onEdit, this::onBlock),
-                new TutorsView(tutorRepository, this::onView, this::onEdit, this::onBlock)
-        );
-
-        tabs.forEach(tab -> {
-            int index = tabs.indexOf(tab);
-            tabSheet.add(new Span(new Span(tabNames.get(index)), badges.get(tab)), tables.get(index));
-        });
-
-        List<RoleDto> roles = roleRepository.findAll().stream()
-                .map(r -> new RoleDto(r.getCode(), r.getName(), r.getDescription()))
-                .toList();
-        UserForm form = new UserForm(roles);
-        driver = new RightDrawer("Редактировать пользователя", form, this::onSave, this::onClose);
-
-        binder.bindInstanceFields(form);
-        binder.forField(form.getRoles())
-                .withConverter(RoleDto::getCode, code -> roleRepository.findByCode(code).map(a -> new RoleDto(a.getCode(), a.getName(), a.getDescription())).orElseThrow())
-                .bind(SimpleUser::getRole, SimpleUser::setRole);
-
-        add(top, tabSheet, driver);
+        return top;
     }
 
-    private void showAcceptMessage() {
-        if (blockIds.size() == 1)
-            Notification.show(ACCEPT_MESSAGE, 3000, Notification.Position.TOP_END);
-        else if (blockIds.size() > 1) {
-            Notification.show(String.format(ACCEPT_ALL_MESSAGE, blockIds.size()), 3000, Notification.Position.TOP_END);
+    private String getCurrentRole() {
+        var selectedTab = tabSheet.getSelectedIndex();
+        if (selectedTab > -1) {
+            return rolesInOrder.get(selectedTab);
         }
+        return "visitor";
     }
-
-    private void showBlockMessage() {
-        if (blockIds.size() == 1)
-            Notification.show(BLOCK_MESSAGE, 3000, Notification.Position.TOP_END);
-        else if (blockIds.size() > 1) {
-            Notification.show(String.format(BLOCK_ALL_MESSAGE, blockIds.size()), 3000, Notification.Position.TOP_END);
-        }
-    }
-
-    private void showRejectMessage() {
-        if (blockIds.size() == 1)
-            Notification.show(REJECT_MESSAGE, 3000, Notification.Position.TOP_END);
-        else if (blockIds.size() > 1) {
-            Notification.show(String.format(REJECT_ALL_MESSAGE, blockIds.size()), 3000, Notification.Position.TOP_END);
-        }
-        blockIds = null;
-        this.provider = null;
-    }
-
-    private void onAccept(List<Long> ids) {
-        dialogAccept.setCount(ids.size());
-        dialogAccept.open();
-    }
-
-    private void onReject(List<Long> ids) {
-        dialogReject.setCount(ids.size());
-        dialogReject.open();
-    }
-
-    private Boolean onSave() {
-        if (binder.validate().isOk()) {
-            provider.save(binder.getBean());
-            provider.refreshAll();
-            this.provider = null;
-            Notification.show("Сохранено", 3000, Notification.Position.TOP_END);
-            return true;
-        }
-        return false;
-    }
-
-    private void onView(Long id, ProviderGet provider) {
-        this.provider = provider;
-        Optional<SimpleUser> simpleUser = provider.findSimpleUserById(id);
-        simpleUser.ifPresent(u -> {
-            binder.setBean(u);
-            driver.setVisible(true);
-        });
-    }
-
-    private void onEdit(Long id, ProviderGet provider) {
-        this.provider = provider;
-        Optional<SimpleUser> simpleUser = provider.findSimpleUserById(id);
-        simpleUser.ifPresent(u -> {
-            binder.setBean(u);
-            driver.setVisible(true);
-        });
-    }
-
-    private void onBlock(List<Long> ids, ProviderGet provider) {
-        this.provider = provider;
-        dialogBlock.setCount(ids.size());
-        dialogBlock.open();
-        blockIds = ids;
-    }
-
-    void onClose() {
-        this.provider = null;
-        binder.setBean(new SimpleUser());
-    }
-
 }
