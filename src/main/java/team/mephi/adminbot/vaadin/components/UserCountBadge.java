d8f970a0c431349a373aaa4f295dc8161e5885e9
Dmitry Dmitriev <d.dmitrieff@gmail.com>
2025-12-29 12:21:38 +0300

    interface v2

--- src/main/java/team/mephi/adminbot/vaadin/components/UserCountBadge.java
+++ src/main/java/team/mephi/adminbot/vaadin/components/UserCountBadge.java
@@ -1,12 +1,13 @@
 package team.mephi.adminbot.vaadin.components;
 
 import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.theme.lumo.LumoUtility;
 
 public class UserCountBadge extends Span {
     public UserCountBadge(Long value) {
         setText(String.valueOf(value));
         getElement().getThemeList().add("badge small contrast");
-        getStyle().set("margin-inline-start", "var(--lumo-space-xs)");
+        addClassNames(LumoUtility.Margin.Left.XSMALL);
     }
 
     public void setCount(Long value) {
