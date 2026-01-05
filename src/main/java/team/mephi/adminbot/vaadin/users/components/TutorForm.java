af05bf920c503be649e6d0ef9b8f0162129f0e8d
Dmitry Dmitriev <d.dmitrieff@gmail.com>
2026-01-05 22:09:35 +0300

    interface v2

--- src/main/java/team/mephi/adminbot/vaadin/users/components/TutorForm.java
+++ src/main/java/team/mephi/adminbot/vaadin/users/components/TutorForm.java
@@ -15,15 +15,15 @@ public class TutorForm extends FormLayout {
     private TextField firstName = new TextField();
     private TextField lastName = new TextField();
     private TextField tgId = new TextField();
-    private ComboBox<SimpleDirection> direction = new ComboBox();
+    private ComboBox<SimpleDirection> direction = new ComboBox<>();
 
     public TutorForm(UserService userService) {
         var provider = new CallbackDataProvider<SimpleUser, String>(
                 query -> {
                     Pageable pageable = PageRequest.of(query.getOffset() / query.getLimit(), query.getLimit());
-                    return userService.findAllByRoleAndName("student", "", pageable);
+                    return userService.findAllByRoleAndName("student", query.getFilter().orElse(""), pageable);
                 },
-                q -> userService.countByRoleAndName("student", ""),
+                query -> userService.countByRoleAndName("student", query.getFilter().orElse("")),
                 SimpleUser::getId
         );
 
@@ -31,17 +31,15 @@ public class TutorForm extends FormLayout {
         setLabelsAside(true);
         setExpandFields(true);
 
+        MultiSelectComboBox<SimpleUser> comboBox = new MultiSelectComboBox<>();
+        comboBox.setItems(provider);
+        comboBox.setItemLabelGenerator(SimpleUser::getFullName);
+        comboBox.setAutoExpand(MultiSelectComboBox.AutoExpandMode.VERTICAL);
+
         addFormItem(firstName, getTranslation("form_tutor_curatorship_first_name_label"));
         addFormItem(lastName, getTranslation("form_tutor_curatorship_last_name_label"));
         addFormItem(tgId, getTranslation("form_tutor_curatorship_telegram_label"));
         addFormItem(direction, getTranslation("form_tutor_curatorship_direction_label"));
-
-        MultiSelectComboBox<SimpleUser> comboBox = new MultiSelectComboBox<>();
-        comboBox.setDataProvider(provider, e -> e);
-        comboBox.setItemLabelGenerator(SimpleUser::getFullName);
-        comboBox.setAutoExpand(MultiSelectComboBox.AutoExpandMode.BOTH);
-        FormItem box = addFormItem(comboBox, getTranslation("form_tutor_curatorship_students_label"));
-
-        add(box);
+        addFormItem(comboBox, getTranslation("form_tutor_curatorship_students_label"));
     }
 }
