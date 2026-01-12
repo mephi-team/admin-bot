1d9e82ac0f71dcf8d47dc09950b804342033e62c
Dmitry Dmitriev <d.dmitrieff@gmail.com>
2026-01-12 15:53:26 +0300

    interface v2

--- src/main/java/team/mephi/adminbot/vaadin/components/UserMenu.java
+++ src/main/java/team/mephi/adminbot/vaadin/components/UserMenu.java
@@ -8,13 +8,17 @@ import com.vaadin.flow.spring.security.AuthenticationContext;
 import com.vaadin.flow.theme.lumo.LumoUtility;
 import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
 
+import java.util.Objects;
+
 public class UserMenu extends HorizontalLayout {
     public UserMenu(AuthenticationContext authenticationContext) {
         addClassNames(LumoUtility.AlignItems.CENTER);
         setPadding(true);
 
         DefaultOidcUser user = authenticationContext.getAuthenticatedUser(DefaultOidcUser.class).orElseThrow();
-        Span userName = new Span(user.getUserInfo().getFullName());
+        String fullName = user.getUserInfo().getFullName();
+        String email = user.getUserInfo().getEmail();
+        Span userName = new Span(Objects.isNull(fullName) ? email : fullName);
         userName.addClassNames(LumoUtility.Whitespace.NOWRAP);
 
         Button logout = new IconButton(VaadinIcon.SIGN_OUT.create(), event -> authenticationContext.logout());
