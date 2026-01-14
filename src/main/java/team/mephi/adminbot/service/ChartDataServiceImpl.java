ff9c49d74cc7a3562826c1811147cfa0df241a21
Dmitry Dmitriev <d.dmitrieff@gmail.com>
2026-01-15 02:08:55 +0300

    interface v2

--- src/main/java/team/mephi/adminbot/service/ChartDataServiceImpl.java
+++ src/main/java/team/mephi/adminbot/service/ChartDataServiceImpl.java
@@ -9,70 +9,76 @@ import team.mephi.adminbot.vaadin.analytics.views.OrdersView;
 import team.mephi.adminbot.vaadin.analytics.views.PreordersView;
 import team.mephi.adminbot.vaadin.analytics.views.UtmView;
 
-import java.util.List;
-import java.util.Random;
-
 @Service
 public class ChartDataServiceImpl implements ChartDataService {
-    private static final List<String> MONTH_LABELS = List.of("Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь");
-    private static final List<String> DAY_LABELS = List.of("12.01.2026", "13.01.2026", "14.01.2026", "15.01.2026", "16.01.2026", "17.01.2026", "18.01.2026", "19.01.2026", "20.01.2026", "21.01.2026", "22.01.2026", "23.01.2026");
-    private static final List<String> HOUR_LABELS = List.of("00:00", "01:00", "02:00", "03:00", "04:00", "05:00", "06:00", "07:00", "08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00");
+    private static final String[] MONTH_LABELS = new String[]{"Январь","Февраль","Март","Апрель","Май","Июнь","Июль","Август","Сентябрь","Октябрь","Ноябрь","Декабрь"};
+    private static final String[] DAY_LABELS = new String[]{"12.01.2026","13.01.2026","14.01.2026","15.01.2026","16.01.2026","17.01.2026","18.01.2026","19.01.2026","20.01.2026","21.01.2026","22.01.2026","23.01.2026"};
+    private static final String[] HOUR_LABELS = new String[]{"00:00","01:00","02:00","03:00","04:00","05:00","06:00","07:00","08:00","09:00","10:00","11:00","12:00","13:00","14:00","15:00","16:00","17:00","18:00","19:00","20:00","21:00","22:00","23:00"};
+    private static final String DEFAULT_BLUE = "#2168df";
 
-    private List<String> labelsForInterval(String interval) {
-        if (ActivityIntervals.HOUR.name().equals(interval)) {
-            return HOUR_LABELS;
-        } else if (ActivityIntervals.DAY.name().equals(interval)) {
-            return DAY_LABELS;
+    private final java.util.Random random = new java.util.Random();
+
+    private String[] labelsForInterval(String interval) {
+        try {
+            ActivityIntervals ai = ActivityIntervals.valueOf(interval);
+            switch (ai) {
+                case HOUR: return HOUR_LABELS;
+                case DAY: return DAY_LABELS;
+                default: return MONTH_LABELS;
+            }
+        } catch (Exception e) {
+            return MONTH_LABELS;
         }
-        return MONTH_LABELS;
     }
 
-    private BarDataset createDataset(String label, String backgroundColor, List<String> labels, Random random) {
+    private BarDataset createDataset(String label, String backgroundColor, String[] labels) {
         BarDataset dataset = new BarDataset().setLabel(label);
         if (backgroundColor != null) {
             dataset.setBackgroundColor(backgroundColor);
         }
-        labels.stream().map(s -> random.nextInt(0, 100)).forEach(dataset::addData);
+        for (int i = 0; i < labels.length; i++) {
+            dataset.addData(random.nextInt(0, 100));
+        }
         return dataset;
+    }
+
+    private record DatasetSpec(String label, String color) {}
+
+    private BarData createBarData(String[] labels, DatasetSpec... specs) {
+        BarData barData = new BarData().addLabels(labels);
+        for (DatasetSpec spec : specs) {
+            barData.addDataset(createDataset(spec.label(), spec.color(), labels));
+        }
+        return barData;
     }
 
     @Override
     public BarData forActivity(ActivityView.ActivityFilterData data) {
-        Random random = new Random();
-        var labels = labelsForInterval(data.getInterval());
-        BarData barData = new BarData().addLabels(labels.toArray(new String[0]));
-        barData.addDataset(createDataset("Активность", "#2168df", labels, random));
-        return barData;
+        String[] labels = labelsForInterval(data.getInterval());
+        return createBarData(labels, new DatasetSpec("Активность", DEFAULT_BLUE));
     }
 
     @Override
     public BarData forPreorders(PreordersView.PreorderFilterData data) {
-        Random random = new Random();
-        var labels = labelsForInterval(data.getInterval());
-        BarData barData = new BarData().addLabels(labels.toArray(new String[0]));
-        barData.addDataset(createDataset("Подано всего заявок", "#2168df", labels, random));
-        barData.addDataset(createDataset("Актуальные заявки", "#d3e1f9", labels, random));
-        barData.addDataset(createDataset("Отозванные заявки", null, labels, random));
-        return barData;
+        String[] labels = labelsForInterval(data.getInterval());
+        return createBarData(labels,
+                new DatasetSpec("Подано всего заявок", DEFAULT_BLUE),
+                new DatasetSpec("Актуальные заявки", "#d3e1f9"),
+                new DatasetSpec("Отозванные заявки", null));
     }
 
     @Override
     public BarData forOrders(OrdersView.OrderFilterData data) {
-        Random random = new Random();
-        var labels = labelsForInterval(data.getInterval());
-        BarData barData = new BarData().addLabels(labels.toArray(new String[0]));
-        barData.addDataset(createDataset("Подано всего заявок", "#2168df", labels, random));
-        barData.addDataset(createDataset("Актуальные заявки", "#d3e1f9", labels, random));
-        barData.addDataset(createDataset("Отозванные заявки", null, labels, random));
-        return barData;
+        String[] labels = labelsForInterval(data.getInterval());
+        return createBarData(labels,
+                new DatasetSpec("Подано всего заявок", DEFAULT_BLUE),
+                new DatasetSpec("Актуальные заявки", "#d3e1f9"),
+                new DatasetSpec("Отозванные заявки", null));
     }
 
     @Override
     public BarData forUtm(UtmView.UtmFilterData data) {
-        Random random = new Random();
-        var labels = labelsForInterval(data.getInterval());
-        BarData barData = new BarData().addLabels(labels.toArray(new String[0]));
-        barData.addDataset(createDataset("UTM", "#2168df", labels, random));
-        return barData;
+        String[] labels = labelsForInterval(data.getInterval());
+        return createBarData(labels, new DatasetSpec("UTM", DEFAULT_BLUE));
     }
 }
