3bea4ca25ea11749bba03de0141f0d25fe568f86
Dmitry Dmitriev <d.dmitrieff@gmail.com>
2026-01-15 01:23:29 +0300

    interface v2

--- src/main/java/team/mephi/adminbot/vaadin/components/fields/DateRangePicker.java
+++ src/main/java/team/mephi/adminbot/vaadin/components/fields/DateRangePicker.java
@@ -8,6 +8,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDate;
+import java.util.Arrays;
 
 public class DateRangePicker extends CustomField<DateRangePicker.LocalDateRange> {
 
@@ -26,6 +27,8 @@ public class DateRangePicker extends CustomField<DateRangePicker.LocalDateRange>
         start = new DatePicker();
         end = new DatePicker();
 
+        setI18n(createRussianI18n());
+
         start.setManualValidation(true);
         end.setManualValidation(true);
 
@@ -39,6 +42,50 @@ public class DateRangePicker extends CustomField<DateRangePicker.LocalDateRange>
         container.setFlexGrow(1.0, start);
 
         add(container);
+    }
+
+    /**
+     * Устанавливает i18n для обоих DatePicker'ов.
+     */
+    public void setI18n(DatePicker.DatePickerI18n i18n) {
+        start.setI18n(i18n);
+        end.setI18n(i18n);
+    }
+
+    private DatePicker.DatePickerI18n createRussianI18n() {
+        return new DatePicker.DatePickerI18n()
+                .setMonthNames(Arrays.asList(
+                        getTranslation("datepicker_months_january"),
+                        getTranslation("datepicker_months_february"),
+                        getTranslation("datepicker_months_march"),
+                        getTranslation("datepicker_months_april"),
+                        getTranslation("datepicker_months_may"),
+                        getTranslation("datepicker_months_june"),
+                        getTranslation("datepicker_months_july"),
+                        getTranslation("datepicker_months_august"),
+                        getTranslation("datepicker_months_september"),
+                        getTranslation("datepicker_months_october"),
+                        getTranslation("datepicker_months_november"),
+                        getTranslation("datepicker_months_december")))
+                .setWeekdays(Arrays.asList(
+                        getTranslation("datepicker_weekdays_sunday"),
+                        getTranslation("datepicker_weekdays_monday"),
+                        getTranslation("datepicker_weekdays_tuesday"),
+                        getTranslation("datepicker_weekdays_wednesday"),
+                        getTranslation("datepicker_weekdays_thursday"),
+                        getTranslation("datepicker_weekdays_friday"),
+                        getTranslation("datepicker_weekdays_saturday")))
+                .setWeekdaysShort(Arrays.asList(
+                        getTranslation("datepicker_weekdays_sun"),
+                        getTranslation("datepicker_weekdays_mon"),
+                        getTranslation("datepicker_weekdays_tue"),
+                        getTranslation("datepicker_weekdays_wed"),
+                        getTranslation("datepicker_weekdays_thu"),
+                        getTranslation("datepicker_weekdays_fri"),
+                        getTranslation("datepicker_weekdays_sat")))
+                .setToday(getTranslation("datepicker_today"))
+                .setCancel(getTranslation("datepicker_cancel"))
+                .setFirstDayOfWeek(1);
     }
 
     public void changeMode(Mode mode) {
