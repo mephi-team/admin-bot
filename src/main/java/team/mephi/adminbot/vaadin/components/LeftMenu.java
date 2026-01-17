1984cf51855bbe9e95710560279b642385b6bf94
Dmitry Dmitriev <d.dmitrieff@gmail.com>
2026-01-17 17:34:55 +0300

    interface v2

--- src/main/java/team/mephi/adminbot/vaadin/components/LeftMenu.java
+++ src/main/java/team/mephi/adminbot/vaadin/components/LeftMenu.java
@@ -20,10 +20,10 @@ public class LeftMenu extends SideNav {
     public LeftMenu(AuthService authService) {
 
         if (authService.isAdmin()) {
-            var item = new SideNavItem(getTranslation("left_menu_dialogs_link"), Dialogs.class, VaadinIcon.DASHBOARD.create());
+            var item = new SideNavItem(getTranslation("left_menu_dialogs_link"), Dialogs.class, VaadinIcon.COMMENT_O.create());
             item.setMatchNested(true);
-            addItem(item, new SideNavItem(getTranslation("left_menu_mailing_link"), Mailings.class, VaadinIcon.CART.create()));
+            addItem(item, new SideNavItem(getTranslation("left_menu_mailing_link"), Mailings.class, VaadinIcon.ENVELOPE_O.create()));
         }
-        addItem(new SideNavItem(getTranslation("left_menu_question_link"), Questions.class, VaadinIcon.USER_HEART.create()));
+        addItem(new SideNavItem(getTranslation("left_menu_question_link"), Questions.class, VaadinIcon.COMMENTS_O.create()));
     }
 }
